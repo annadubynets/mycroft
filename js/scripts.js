1ca2c3c7bc67e75c9be3bb12076ea28242afc04d
Anna Dubynets <arogulina@gmail.com>
2021-10-11 10:16:44 +0300

    fix navbar (mobile)

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,24 @@ $(function() {
         $('#' + collapsedId).removeClass('d-none');
     });
 })
+
+var NavbarTools = NavbarTools || {}
+
+NavbarTools.startScrollTracking = function() {
+    var $nav = $(".main-navbar");
+    if ($nav.length > 0) {
+        $(window).on('scroll', function() {
+            markNavbarScrolledIfNeeded();
+        });
+
+        markNavbarScrolledIfNeeded();
+    }
+
+    function markNavbarScrolledIfNeeded() {
+        $nav.toggleClass('scrolled', $(this).scrollTop() != 0);
+    }
+}
+
+$(function() {
+    NavbarTools.startScrollTracking();
+})
